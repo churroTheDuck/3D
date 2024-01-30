b61ac3b543c60cdc7e413e607982ee767f167115
Lang <langchenca@gmail.com>
2024-01-29 18:15:00 -0800

    Added aiming

--- script.js
+++ script.js
@@ -2,8 +2,14 @@ var bulletsX = [];
 var bulletsY = [];
 var bulletsZ = [];
 var bulletsXDir = [];
-var bulletsZDir = [];
+var bulletsYDir = [];
 var shoot = false;
+var left = false;
+var right = false;
+var up = false;
+var down = false;
+var directionX = 0;
+var directionY = 0
 
 function preload() {
 	backgroundImg = loadImage("background.jpg");  
@@ -21,31 +27,42 @@ function setup() {
 
 function draw() {
 	push();
+	if (left) {
+		directionX -= 0.01;
+	}
+	if (right) {
+		directionX += 0.01;
+	}
+	if (up) {
+		directionY += 0.01;
+	}
+	if (down) {
+		directionY -= 0.01;
+	}
 	if (shoot) {
 		bulletsX.push(0);
 		bulletsY.push(200);
-		bulletsZ.push(0);
-		bulletsXDir.push();
-		bulletsZDir.push();
+		bulletsZ.push(200);
+		bulletsXDir.push(directionX + Math.random() / 50);
+		bulletsYDir.push(directionY + Math.random() / 50);
 	}
 	pop();
 	game.background(0);
-	game.push();
+	/*game.push();
 	game.translate(0, 0, 10);
 	game.noFill();
 	game.stroke("green");
 	game.strokeWeight(1);
 	game.torus(10, 1, 16, 1);
-	game.pop();
+	game.pop();*/
 	for (var i = 0; i < bulletsX.length; i++) {
 		game.push();
 		game.translate(bulletsX[i], bulletsY[i], bulletsZ[i]);
 		game.fill("white");
 		game.noStroke();
-		game.box(5);
-		//bulletsX[i] += Math.sin(bulletsXDir[i]);
-		//bulletsY[i] += Math.cos(bulletsZDir[i]);
-		bulletsY[i] -= 1
+		game.sphere(5);
+		bulletsX[i] += Math.sin(bulletsXDir[i]) * 50;
+		bulletsY[i] += Math.cos(bulletsYDir[i]) * 10;
 		bulletsZ[i] -= 50;
 		if (bulletsY[i] >= 50000) {
 			bulletsX.splice(i, 1);
@@ -64,10 +81,34 @@ function draw() {
 		if (key == " ") {
 			shoot = true;
 		}
+		if (key == "a") {
+			left = true;
+		}
+		if (key == "d") {
+			right = true;
+		}
+		if (key == "w") {
+			up = true;
+		}
+		if (key == "s") {
+			down = true;
+		}
 	}
 
 	function keyReleased() {
 		if (key == " ") {
 			shoot = false;
 		}		
+			if (key == "a") {
+			left = false;
+		}
+		if (key == "d") {
+			right = false;
+		}	
+		if (key == "w") {
+			up = false;
+		}
+		if (key == "s") {
+			down = false;
+		}
 	}
